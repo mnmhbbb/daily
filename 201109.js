94be8f2842b40c33b68c3dae24bebf1eeb01c801
mnmhbbb <mnmhb64@naver.com>
2020-11-10 01:24:35 +0900

    201109 Youtube clone coding
    Youtube html&css clone coding(w. dreamcoding)

---
+++ 201109.js
@@ -0,0 +1,7 @@
+const moreBtn = document.querySelector('.titleAndButton .moreBtn');
+const title = document.querySelector('.titleAndButton .title') 
+
+moreBtn.addEventListener('click', ()=>{
+    moreBtn.classList.toggle('clicked')
+    title.classList.toggle('clamp')
+})
