221665c8ca060d25ed56d80113b3ddd557c91fbc
mnmhbbb <mnmhb64@naver.com>
2020-11-19 23:38:20 +0900

    201119
    다시 연습

--- 201109.js
+++ 201109.js
@@ -1,7 +1,7 @@
-const moreBtn = document.querySelector('.titleAndButton .moreBtn');
-const title = document.querySelector('.titleAndButton .title') 
+const moreBtn = document.querySelector(".titleAndButton .moreBtn");
+const title = document.querySelector(".titleAndButton .title");
 
-moreBtn.addEventListener('click', ()=>{
-    moreBtn.classList.toggle('clicked')
-    title.classList.toggle('clamp')
-})
+moreBtn.addEventListener("click", () => {
+  moreBtn.classList.toggle("clicked");
+  title.classList.toggle("clamp");
+});
