c9124145ccbb8a6132bcb1ee44b84608eeda6f42
mnmhbbb <mnmhb64@naver.com>
2020-11-22 07:04:57 +0900

    201122
    로또추첨기
    setTimeout, 변수명 주의

---
+++ 201122.js
@@ -0,0 +1,49 @@
+let number = Array(45)
+  .fill()
+  .map(function (item, index) {
+    return index + 1;
+  });
+
+let lotto = [];
+for (i = 0; i < 6; i++) {
+  let shuffle = number.splice(Math.floor(Math.random() * number.length), 1)[0];
+  lotto.push(shuffle);
+}
+
+let bonus = lotto.splice(5, 1);
+
+lotto.sort(function (a, b) {
+  return a - b;
+});
+
+let result = document.querySelector(".result");
+
+function colorBall(num, result) {
+  let ball = document.createElement("div");
+  ball.textContent = num;
+  ball.className = "ball";
+  let back;
+
+  if (num <= 10) {
+    back = "red";
+  } else if (num <= 20) {
+    back = "orange";
+  } else if (num <= 30) {
+    back = "yellow";
+  } else if (num <= 40) {
+    back = "blue";
+  } else {
+    back = "green";
+  }
+  ball.style.background = back;
+  result.appendChild(ball);
+}
+
+for (j = 0; j < lotto.length; j++) {
+  colorBall(lotto[j], result);
+}
+
+setTimeout(function () {
+  let bonusBall = document.querySelector(".bonus");
+  colorBall(bonus[0], bonusBall);
+}, 1000);
