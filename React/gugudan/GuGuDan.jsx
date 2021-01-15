f44da3909d9dda69ff33108f2185caaa103d9e49
mnmhbbb <mnmhb64@naver.com>
2021-01-16 02:42:03 +0900

    gugudan webpack

---
+++ React/gugudan/GuGuDan.jsx
@@ -0,0 +1,48 @@
+const React = require("react");
+const { useState, useRef } = React;
+
+const GuGuDan = () => {
+  const [first, setFirst] = useState(Math.ceil(Math.random() * 9));
+  const [second, setSecond] = useState(Math.ceil(Math.random() * 9));
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+  const inputRef = useRef(null);
+
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (parseInt(value) === first * second) {
+      setResult(value + " 정답!");
+      setValue("");
+      setFirst(Math.ceil(Math.random() * 9));
+      setSecond(Math.ceil(Math.random() * 9));
+      inputRef.current.focus();
+    } else {
+      setResult("땡! 다시 입력하세요");
+      setValue("");
+      inputRef.current.focus();
+    }
+  };
+  return (
+    <>
+      <div>
+        {first}곱하기{second}
+      </div>
+      <form onSubmit={onSubmitForm}>
+        <input
+          type="number"
+          value={value}
+          onChange={onChangeInput}
+          ref={inputRef}
+        />
+        <button>입력</button>
+      </form>
+      <div id="result">{result}</div>
+    </>
+  );
+};
+
+module.exports = GuGuDan;
