435b61460295958a8e26eca6b1d882cb27a3b1cd
mnmhbbb <mnmhb64@naver.com>
2021-02-13 23:58:13 +0900

    front

--- React/react-nodebird/prepare/front/components/FollowButton.js
+++ React/react-nodebird/prepare/front/components/FollowButton.js
@@ -1,12 +1,14 @@
-import React, { useCallback } from 'react';
-import { Button } from 'antd';
-import PropTypes from 'prop-types';
-import { useSelector, useDispatch } from 'react-redux';
-import { FOLLOW_REQUEST, UNFOLLOW_REQUEST } from '../reducers/user';
+import React, { useCallback } from "react";
+import { Button } from "antd";
+import PropTypes from "prop-types";
+import { useSelector, useDispatch } from "react-redux";
+import { FOLLOW_REQUEST, UNFOLLOW_REQUEST } from "../reducers/user";
 
 const FollowButton = ({ post }) => {
   const dispatch = useDispatch();
-  const { me, followLoading, unfollowLoading } = useSelector((state) => state.user);
+  const { me, followLoading, unfollowLoading } = useSelector(
+    (state) => state.user
+  );
   const isFollowing = me?.Followings.find((v) => v.id === post.User.id);
   const onClickButton = useCallback(() => {
     if (isFollowing) {
@@ -24,13 +26,16 @@ const FollowButton = ({ post }) => {
 
   return (
     <Button loading={followLoading || unfollowLoading} onClick={onClickButton}>
-      {isFollowing ? '언팔로우' : '팔로우'}
+      {isFollowing ? "언팔로우" : "팔로우"}
     </Button>
   );
 };
 
 FollowButton.propTypes = {
-  post: PropTypes.object.isRequired,
+  post: PropTypes.shape({
+    id: PropTypes.number,
+    User: PropTypes.object,
+  }).isRequired,
 };
 
 export default FollowButton;
--- React/react-nodebird/prepare/front/components/PostCard.js
+++ React/react-nodebird/prepare/front/components/PostCard.js
@@ -1,16 +1,22 @@
-import React, { useState, useCallback } from 'react';
-import { Card, Button, Avatar, List, Comment, Popover } from 'antd';
-import PropTypes from 'prop-types';
-import { RetweetOutlined, HeartTwoTone, HeartOutlined, MessageOutlined, EllipsisOutlined } from '@ant-design/icons';
-import { useSelector, useDispatch } from 'react-redux';
-import styled from 'styled-components';
-import Link from 'next/link';
+import React, { useState, useCallback } from "react";
+import { Card, Button, Avatar, List, Comment, Popover } from "antd";
+import PropTypes from "prop-types";
+import {
+  RetweetOutlined,
+  HeartTwoTone,
+  HeartOutlined,
+  MessageOutlined,
+  EllipsisOutlined,
+} from "@ant-design/icons";
+import { useSelector, useDispatch } from "react-redux";
+import styled from "styled-components";
+import Link from "next/link";
 
-import CommentForm from './CommentForm';
-import PostCardContent from './PostCardContent';
-import PostImages from './PostImages';
-import FollowButton from './FollowButton';
-import { REMOVE_POST_REQUEST } from '../reducers/post';
+import CommentForm from "./CommentForm";
+import PostCardContent from "./PostCardContent";
+import PostImages from "./PostImages";
+import FollowButton from "./FollowButton";
+import { REMOVE_POST_REQUEST } from "../reducers/post";
 
 const CardWrapper = styled.div`
   margin-bottom: 20px;
@@ -30,6 +36,7 @@ const PostCard = ({ post }) => {
 
   const onToggleLike = useCallback(() => {
     setLiked((prev) => !prev);
+    console.log(id, post.UserId);
   }, []);
 
   const onRemovePost = useCallback(() => {
@@ -45,24 +52,36 @@ const PostCard = ({ post }) => {
         cover={post.Images[0] && <PostImages images={post.Images} />}
         actions={[
           <RetweetOutlined key="retweet" />,
-          liked
-            ? <HeartTwoTone twoToneColor="#eb2f96" key="heart" onClick={onToggleLike} />
-            : <HeartOutlined key="heart" onClick={onToggleLike} />,
+          liked ? (
+            <HeartTwoTone
+              twoToneColor="#eb2f96"
+              key="heart"
+              onClick={onToggleLike}
+            />
+          ) : (
+            <HeartOutlined key="heart" onClick={onToggleLike} />
+          ),
           <MessageOutlined key="message" onClick={onToggleComment} />,
           <Popover
             key="ellipsis"
-            content={(
+            content={
               <Button.Group>
-                {id && post.UserId === id
-                  ? (
-                    <>
-                      <Button>수정</Button>
-                      <Button type="danger" loading={removePostLoading} onClick={onRemovePost}>삭제</Button>
-                    </>
-                  )
-                  : <Button>신고</Button>}
+                {id && post.UserId === id ? (
+                  <>
+                    <Button>수정</Button>
+                    <Button
+                      type="danger"
+                      loading={removePostLoading}
+                      onClick={onRemovePost}
+                    >
+                      삭제
+                    </Button>
+                  </>
+                ) : (
+                  <Button>신고</Button>
+                )}
               </Button.Group>
-            )}
+            }
           >
             <EllipsisOutlined />
           </Popover>,
@@ -86,11 +105,16 @@ const PostCard = ({ post }) => {
               <li>
                 <Comment
                   author={item.User.nickname}
-                  avatar={(
-                    <Link href={{ pathname: '/user', query: { id: item.User.id } }} as={`/user/${item.User.id}`}>
-                      <a><Avatar>{item.User.nickname[0]}</Avatar></a>
+                  avatar={
+                    <Link
+                      href={{ pathname: "/user", query: { id: item.User.id } }}
+                      as={`/user/${item.User.id}`}
+                    >
+                      <a>
+                        <Avatar>{item.User.nickname[0]}</Avatar>
+                      </a>
                     </Link>
-                  )}
+                  }
                   content={item.content}
                 />
               </li>
