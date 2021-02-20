ae956b4e481e3ea5ea7fd3ab1542b3b21dfe42b2
mnmhbbb <mnmhb64@naver.com>
2021-02-20 12:49:15 +0900

    full stack study

--- React/react-nodebird/prepare/back/routes/user.js
+++ React/react-nodebird/prepare/back/routes/user.js
@@ -1,45 +1,50 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const passport = require('passport');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
 
-const { User, Post } = require('../models');
-const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { User, Post } = require("../models");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => { // GET /user
-    try {
-      if (req.user) {
-        const fullUserWithoutPassword = await User.findOne({
-          where: { id: req.user.id },
-          attributes: {
-            exclude: ['password']
-          },
-          include: [{
+router.get("/", async (req, res, next) => {
+  // GET /user
+  try {
+    if (req.user) {
+      const fullUserWithoutPassword = await User.findOne({
+        where: { id: req.user.id },
+        attributes: {
+          exclude: ["password"],
+        },
+        include: [
+          {
             model: Post,
-            attributes: ['id'],
-          }, {
+            attributes: ["id"],
+          },
+          {
             model: User,
-            as: 'Followings',
-            attributes: ['id'],
-          }, {
+            as: "Followings",
+            attributes: ["id"],
+          },
+          {
             model: User,
-            as: 'Followers',
-            attributes: ['id'],
-          }]
-        })
-        res.status(200).json(fullUserWithoutPassword);
-      } else {
-        res.status(200).json(null);
-      }
-    } catch (error) {
-      console.error(error);
-   next(error);
+            as: "Followers",
+            attributes: ["id"],
+          },
+        ],
+      });
+      res.status(200).json(fullUserWithoutPassword);
+    } else {
+      res.status(200).json(null);
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
   }
 });
 
-router.post('/login', isNotLoggedIn, (req, res, next) => {
-  passport.authenticate('local', (err, user, info) => {
+router.post("/login", isNotLoggedIn, (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
     if (err) {
       console.error(err);
       return next(err);
@@ -55,35 +60,40 @@ router.post('/login', isNotLoggedIn, (req, res, next) => {
       const fullUserWithoutPassword = await User.findOne({
         where: { id: user.id },
         attributes: {
-          exclude: ['password']
+          exclude: ["password"],
         },
-        include: [{
-          model: Post,
-          attributes: ['id'],
-        }, {
-          model: User,
-          as: 'Followings',
-          attributes: ['id'],
-        }, {
-          model: User,
-          as: 'Followers',
-          attributes: ['id'],
-        }]
-      })
+        include: [
+          {
+            model: Post,
+            attributes: ["id"],
+          },
+          {
+            model: User,
+            as: "Followings",
+            attributes: ["id"],
+          },
+          {
+            model: User,
+            as: "Followers",
+            attributes: ["id"],
+          },
+        ],
+      });
       return res.status(200).json(fullUserWithoutPassword);
     });
   })(req, res, next);
 });
 
-router.post('/', isNotLoggedIn, async (req, res, next) => { // POST /user/
+router.post("/", isNotLoggedIn, async (req, res, next) => {
+  // POST /user/
   try {
     const exUser = await User.findOne({
       where: {
         email: req.body.email,
-      }
+      },
     });
     if (exUser) {
-      return res.status(403).send('이미 사용 중인 아이디입니다.');
+      return res.status(403).send("이미 사용 중인 아이디입니다.");
     }
     const hashedPassword = await bcrypt.hash(req.body.password, 12);
     await User.create({
@@ -91,26 +101,29 @@ router.post('/', isNotLoggedIn, async (req, res, next) => { // POST /user/
       nickname: req.body.nickname,
       password: hashedPassword,
     });
-    res.status(201).send('ok');
+    res.status(201).send("ok");
   } catch (error) {
     console.error(error);
     next(error); // status 500
   }
 });
 
-router.post('/logout', isLoggedIn, (req, res) => {
+router.post("/logout", isLoggedIn, (req, res) => {
   req.logout();
   req.session.destroy();
-  res.send('ok');
+  res.send("ok");
 });
 
-router.patch('/nickname', isLoggedIn, async (req, res, next) => {
+router.patch("/nickname", isLoggedIn, async (req, res, next) => {
   try {
-    await User.update({
-      nickname: req.body.nickname,
-    }, {
-      where: { id: req.user.id },
-    });
+    await User.update(
+      {
+        nickname: req.body.nickname,
+      },
+      {
+        where: { id: req.user.id },
+      }
+    );
     res.status(200).json({ nickname: req.body.nickname });
   } catch (error) {
     console.error(error);
@@ -118,11 +131,12 @@ router.patch('/nickname', isLoggedIn, async (req, res, next) => {
   }
 });
 
-router.patch('/:userId/follow', isLoggedIn, async (req, res, next) => { // PATCH /user/1/follow
+router.patch("/:userId/follow", isLoggedIn, async (req, res, next) => {
+  // PATCH /user/1/follow
   try {
-    const user = await User.findOne({ where: { id: req.params.userId }});
+    const user = await User.findOne({ where: { id: req.params.userId } });
     if (!user) {
-      res.status(403).send('없는 사람을 팔로우하려고 하시네요?');
+      res.status(403).send("팔로우할 사용자가 없습니다.");
     }
     await user.addFollowers(req.user.id);
     res.status(200).json({ UserId: parseInt(req.params.userId, 10) });
@@ -132,11 +146,12 @@ router.patch('/:userId/follow', isLoggedIn, async (req, res, next) => { // PATCH
   }
 });
 
-router.delete('/:userId/follow', isLoggedIn, async (req, res, next) => { // DELETE /user/1/follow
+router.delete("/:userId/follow", isLoggedIn, async (req, res, next) => {
+  // DELETE /user/1/follow
   try {
-    const user = await User.findOne({ where: { id: req.params.userId }});
+    const user = await User.findOne({ where: { id: req.params.userId } });
     if (!user) {
-      res.status(403).send('없는 사람을 언팔로우하려고 하시네요?');
+      res.status(403).send("언팔로우할 사용자가 없습니다.");
     }
     await user.removeFollowers(req.user.id);
     res.status(200).json({ UserId: parseInt(req.params.userId, 10) });
@@ -146,11 +161,12 @@ router.delete('/:userId/follow', isLoggedIn, async (req, res, next) => { // DELE
   }
 });
 
-router.delete('/follower/:userId', isLoggedIn, async (req, res, next) => { // DELETE /user/follower/2
+router.delete("/follower/:userId", isLoggedIn, async (req, res, next) => {
+  // DELETE /user/follower/2
   try {
-    const user = await User.findOne({ where: { id: req.params.userId }});
+    const user = await User.findOne({ where: { id: req.params.userId } });
     if (!user) {
-      res.status(403).send('없는 사람을 차단하려고 하시네요?');
+      res.status(403).send("없는 사람을 차단하려고 하시네요?");
     }
     await user.removeFollowings(req.user.id);
     res.status(200).json({ UserId: parseInt(req.params.userId, 10) });
@@ -160,11 +176,12 @@ router.delete('/follower/:userId', isLoggedIn, async (req, res, next) => { // DE
   }
 });
 
-router.get('/followers', isLoggedIn, async (req, res, next) => { // GET /user/followers
+router.get("/followers", isLoggedIn, async (req, res, next) => {
+  // GET /user/followers
   try {
-    const user = await User.findOne({ where: { id: req.user.id }});
+    const user = await User.findOne({ where: { id: req.user.id } });
     if (!user) {
-      res.status(403).send('없는 사람을 찾으려고 하시네요?');
+      res.status(403).send("없는 사람을 찾으려고 하시네요?");
     }
     const followers = await user.getFollowers();
     res.status(200).json(followers);
@@ -174,11 +191,12 @@ router.get('/followers', isLoggedIn, async (req, res, next) => { // GET /user/fo
   }
 });
 
-router.get('/followings', isLoggedIn, async (req, res, next) => { // GET /user/followings
+router.get("/followings", isLoggedIn, async (req, res, next) => {
+  // GET /user/followings
   try {
-    const user = await User.findOne({ where: { id: req.user.id }});
+    const user = await User.findOne({ where: { id: req.user.id } });
     if (!user) {
-      res.status(403).send('없는 사람을 찾으려고 하시네요?');
+      res.status(403).send("없는 사람을 찾으려고 하시네요?");
     }
     const followings = await user.getFollowings();
     res.status(200).json(followings);
