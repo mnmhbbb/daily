6aa04e87048111a0ea4ec2caf33c8e4236c76eac
mnmhbbb <mnmhb64@naver.com>
2021-03-12 04:23:25 +0900

    logout axios

--- React/basic/client/src/components/views/LandingPage/LandingPage.js
+++ React/basic/client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,36 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response.data));
   }, []);
 
-  return <div>LandingPage</div>;
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`).then((res) => {
+      console.log(res.data);
+      if (res.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃 실패");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h1>Hello</h1>
+      <button onClick={onClickHandler}>Logout</button>
+    </div>
+  );
 }
 
 export default LandingPage;
