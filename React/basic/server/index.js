fa0f127500092a29abb62c69b3bb9eab849bb6fb
mnmhbbb <mnmhb64@naver.com>
2021-03-11 20:54:42 +0900

    lecture note

--- React/basic/server/index.js
+++ React/basic/server/index.js
@@ -26,7 +26,7 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello World!~~ "));
 
-app.get("/api/hello", (req, res) => res.send("Hello World!~~ "));
+app.get("/api/hello", (req, res) => res.send("axios 테스트~!"));
 
 app.post("/api/users/register", (req, res) => {
   //회원 가입 할떄 필요한 정보들을  client에서 가져오면
