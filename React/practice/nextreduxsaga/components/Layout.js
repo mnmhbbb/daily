a4963055d3771add4fbbeaf2f074fabc52229cfa
mnmhbbb <mnmhb64@gmail.com>
2021-04-12 23:39:09 +0900

    이전 내용 복습

--- React/practice/nextreduxsaga/components/Layout.js
+++ React/practice/nextreduxsaga/components/Layout.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import Link from 'next/link'
-import PropTypes from 'prop-types'
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+import { Menu } from 'antd';
 
-const Layout = ({children}) => {
-    return(
-        <div>
-            <div>
-                <Link href="/"><a>홈</a></Link>
-                <Link href="/profile"><a>프로필</a></Link>
-                <Link href="/login"><a>로그인</a></Link>
-            </div>
-            {children}
-        </div>
-    )
-}
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">
+            <a>회원가입</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
+      {children}
+    </div>
+  );
+};
 
 Layout.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
-  
 
 export default Layout;
