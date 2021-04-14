e11b2bd0f207cf12f160b0012d7d550a86e31a80
mnmhbbb <mnmhb64@gmail.com>
2021-04-14 19:07:34 +0900

    🔧 setting

---
+++ ts/setting/ts/client.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+
+import App from './layouts/App';
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3090';
+
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector('#app'),
+);
