15a7baee2e5997942cb5336eb63fdbf18568f9bc
mnmhbbb <mnmhb64@gmail.com>
2021-05-16 21:43:48 +0900

    heroku test

---
+++ deploy-test/utils/importData.js
@@ -0,0 +1,38 @@
+require("dotenv").config({ path: "./config.env" });
+const fs = require("fs");
+import path from 'path';
+const Post = require("../models/Post");
+const connectDB = require("../config/db");
+
+connectDB();
+
+const __dirname = path.resolve();
+const posts = JSON.parse(fs.readFileSync(`${__dirname}/posts.json`, "utf-8"));
+
+const importData = async () => {
+  try {
+    await Post.create(posts);
+    console.log("Data Successfully imported 👌");
+    process.exit();
+  } catch (error) {
+    console.log(`ERROR 💥: ${error}`);
+    process.exit(1);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Post.deleteMany({});
+    console.log("Data successfully deleted");
+    process.exit();
+  } catch (error) {
+    console.log(`ERROR 💥: ${error}`);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteData();
+}
