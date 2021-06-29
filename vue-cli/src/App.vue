226c789b590bda04aa6d0cdee34eacecd4a74013
mnmhbbb <mnmhb64@gmail.com>
2021-06-29 23:47:56 +0900

    vue-cli basic

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm.vue';
+export default {
+  data: function() {
+    return {
+      str: 'Header!',
+    };
+  },
+  components: {
+    LoginForm,
+  },
+};
+</script>
+
+<style></style>
---
+++ vue-cli/src/components/LoginForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <label for="username">ID: </label>
+    <input for="username" type="text" v-model="id" />
+    <label for="password">PW: </label>
+    <input for="password" type="password" v-model="pw" />
+    <label for="check">check</label>
+    <input for="check" type="checkbox" v-model="check" />
+    <button>Login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      id: '',
+      pw: '',
+      check: '',
+    };
+  },
+  methods: {
+    submitForm: function() {
+      console.log(this.id, this.pw);
+      const url = 'https://jsonplaceholder.typicode.com/users';
+      const data = {
+        username: this.id,
+        password: this.pw,
+        check: this.check,
+      };
+
+      axios
+        .post(url, data)
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
