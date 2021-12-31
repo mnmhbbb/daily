120abb902b640d09e59f9399feb5de0122aefe05
mnmhbbb <mnmhb64@gmail.com>
2022-01-01 01:47:02 +0900

    버튼 클릭하여 이미지 위아래로 이동시키기

--- practice_code/KeyboardEvent/main.js
+++ practice_code/KeyboardEvent/main.js
@@ -1,6 +1,6 @@
+const tool = document.getElementById('tool');
 const editor = document.getElementById('editor');
 const imgInput = document.querySelector('.img-input');
-const tool = document.getElementById('tool');
 
 editor.focus();
 
@@ -14,29 +14,32 @@ imgInput.addEventListener('change', function (e) {
 function insertImageDate(file) {
   const reader = new FileReader();
   reader.addEventListener('load', function (e) {
+    let p = document.createElement('p');
     let img = document.createElement('img');
-    editor.append(img);
+    p.append(img);
+    editor.append(p);
     img.src = reader.result;
-    editor.innerHTML += '<div class="nextLine"><br></div>';
+    editor.innerHTML += '<p><br></p>';
     editor.focus();
-    // focus()를 이용하면 커서가 맨 앞으로 들어간다는 문제
   });
   reader.readAsDataURL(file); // base64 encoding
 }
 
-// 이미지 이동 버튼
+// 첨부된 이미지 클릭 시 이미지 이동 버튼 추가
 editor.addEventListener('click', function (e) {
-  const imgMove = document.createElement('div');
-  imgMove.setAttribute('class', 'img-move-btn');
-  imgMove.innerHTML = `
-  <button data-move="up">위로</button>
-  <button data-move="down">아래로</button>
-  `;
+  // 기존에 선택된 이미지가 있을 경우 초기화
+  document.querySelector('.clicked')?.classList.remove('clicked');
 
+  // 현재 선택된 이미지가 있을 때
   if (e.target.tagName == 'IMG') {
-    e.target.classList.add('clicked');
+    const imgMove = document.createElement('div');
+    imgMove.setAttribute('class', 'img-move-btn');
+    imgMove.innerHTML = `
+      <button data-move="up">위로</button>
+      <button data-move="down">아래로</button>
+    `;
+    e.target.parentNode.classList.add('clicked');
     if (!tool.hasChildNodes()) {
-      console.log('o');
       tool.appendChild(imgMove);
     }
   } else {
@@ -44,14 +47,24 @@ editor.addEventListener('click', function (e) {
   }
 });
 
+// 위로 / 아래로 버튼 클릭해서 이미지 이동시키기
 tool.addEventListener('click', function (e) {
-  const clickedImg = document.querySelector('.clicked');
-  console.log(clickedImg);
   if (e.target.tagName == 'BUTTON') {
+    const clickedImg = document.querySelector('.clicked');
     const direction = e.target.dataset.move;
-    console.log(direction);
     if (direction == 'up') {
-      // editor.insertBefore(clickedImg)
+      // previousSibling가 아닌 previousElementSibling를 사용해서
+      // 공백, 줄바꿈 등을 포함하지 않는 '이전 엘리먼트'를 가져올 수 있음
+      const prevElem = clickedImg.previousElementSibling;
+      if (prevElem != null) {
+        // editor.insertBefore(clickedImg, prevElem);
+        prevElem.before(clickedImg);
+      }
+    } else {
+      const nextElem = clickedImg.nextElementSibling;
+      if (nextElem != null) {
+        nextElem.after(clickedImg);
+      }
     }
   }
 });
