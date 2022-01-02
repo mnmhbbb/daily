de5309920837d888ff39ce546186cf0aeba56379
mnmhbbb <mnmhb64@gmail.com>
2022-01-02 21:33:04 +0900

    ♻️ code refactoring

--- practice_code/KeyboardEvent/main.js
+++ practice_code/KeyboardEvent/main.js
@@ -1,32 +1,40 @@
 const tool = document.getElementById('tool');
 const editor = document.getElementById('editor');
 const imgInput = document.querySelector('.img-input');
+const reader = new FileReader();
 
 editor.focus();
 
-imgInput.addEventListener('change', function (e) {
-  const files = e.target.files;
-  if (!!files) {
-    insertImageDate(files[0]);
-  }
-});
+imgInput.addEventListener('change', (e) => onChange(e));
 
-function insertImageDate(file) {
-  const reader = new FileReader();
-  reader.addEventListener('load', function (e) {
-    let p = document.createElement('p');
-    let img = document.createElement('img');
-    p.append(img);
-    editor.append(p);
-    img.src = reader.result;
-    editor.innerHTML += '<p><br></p>';
-    editor.focus();
-  });
+// 첨부된 이미지 클릭 시 이미지 이동 버튼 추가
+editor.addEventListener('click', (e) => addBtn(e));
+
+// 위로 / 아래로 버튼 클릭해서 이미지 이동시키기
+tool.addEventListener('click', (e) => moveImg(e));
+
+function insertImageData(file) {
+  reader.addEventListener('load', readImg);
   reader.readAsDataURL(file); // base64 encoding
 }
 
-// 첨부된 이미지 클릭 시 이미지 이동 버튼 추가
-editor.addEventListener('click', function (e) {
+function onChange(e) {
+  const files = e.target.files;
+  if (!!files) {
+    insertImageData(files[0]);
+  }
+}
+
+function readImg() {
+  const p = document.createElement('p');
+  const img = document.createElement('img');
+  p.append(img);
+  editor.append(p);
+  img.src = reader.result;
+  editor.innerHTML += '<p><br></p>';
+}
+
+function addBtn(e) {
   // 기존에 선택된 이미지가 있을 경우 초기화
   document.querySelector('.clicked')?.classList.remove('clicked');
 
@@ -35,8 +43,8 @@ editor.addEventListener('click', function (e) {
     const imgMove = document.createElement('div');
     imgMove.setAttribute('class', 'img-move-btn');
     imgMove.innerHTML = `
-      <button data-move="up">위로</button>
-      <button data-move="down">아래로</button>
+      <button type="button" data-move="up">위로</button>
+      <button type="button" data-move="down">아래로</button>
     `;
     e.target.parentNode.classList.add('clicked');
     if (!tool.hasChildNodes()) {
@@ -45,16 +53,13 @@ editor.addEventListener('click', function (e) {
   } else {
     tool.innerHTML = '';
   }
-});
+}
 
-// 위로 / 아래로 버튼 클릭해서 이미지 이동시키기
-tool.addEventListener('click', function (e) {
+function moveImg(e) {
   if (e.target.tagName == 'BUTTON') {
     const clickedImg = document.querySelector('.clicked');
     const direction = e.target.dataset.move;
     if (direction == 'up') {
-      // previousSibling가 아닌 previousElementSibling를 사용해서
-      // 공백, 줄바꿈 등을 포함하지 않는 '이전 엘리먼트'를 가져올 수 있음
       const prevElem = clickedImg.previousElementSibling;
       if (prevElem != null) {
         // editor.insertBefore(clickedImg, prevElem);
@@ -67,4 +72,4 @@ tool.addEventListener('click', function (e) {
       }
     }
   }
-});
+}
