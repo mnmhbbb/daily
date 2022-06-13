068f876e3cac821fcfc8a2f09978db7dbf9f0d6c
mnmhbbb <mnmhb64@gmail.com>
2022-06-13 19:16:44 +0900

    react test: add react to a website

---
+++ React/add-react-to-a-website/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const App = () => {
+  return <h1>test</h1>;
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
