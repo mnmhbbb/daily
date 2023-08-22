0dc629318da61cc989df85c9b44e8698a6c5e968
mnmhbbb <mnmhb64@gmail.com>
2023-08-23 00:50:40 +0900

    ✨ add ISR feature

--- practice_code/next-13-test/src/app/products/page.tsx
+++ practice_code/next-13-test/src/app/products/page.tsx
@@ -1,6 +1,9 @@
 import { ProductsType, getProducts } from '@/service/products';
 import Link from 'next/link';
 
+// ISR
+export const revalidate = 3;
+
 const ProductsPage = async () => {
   // 서버(데이터베이스)에 있는 제품 목록을 읽어와서 렌더링
   const products: ProductsType[] = await getProducts();
