c00046c32e389c9b686ad6e03ee1758c5599b047
mnmhbbb <mnmhb64@gmail.com>
2023-08-23 00:51:39 +0900

    ✨ SSR feature using fetch

--- practice_code/next-13-test/src/app/products/page.tsx
+++ practice_code/next-13-test/src/app/products/page.tsx
@@ -2,11 +2,21 @@ import { ProductsType, getProducts } from '@/service/products';
 import Link from 'next/link';
 
 // ISR
-export const revalidate = 3;
+// export const revalidate = 3;
 
 const ProductsPage = async () => {
   // 서버(데이터베이스)에 있는 제품 목록을 읽어와서 렌더링
   const products: ProductsType[] = await getProducts();
+
+  // fetch SSR
+  const res = await fetch('https://meowfacts.herokuapp.com', {
+    // next: {
+    //   revalidate: 3,
+    // },
+    cache: 'no-store',
+  });
+  const data = await res.json();
+  const factText = data.data[0];
 
   return (
     <>
@@ -18,6 +28,7 @@ const ProductsPage = async () => {
           </li>
         ))}
       </ul>
+      <article>{factText}</article>
     </>
   );
 };
