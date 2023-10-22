d7e8e1aac9c2fcd090f514115bf403400c82a3b7
mnmhbbb <mnmhb64@gmail.com>
2023-10-22 20:38:59 +0900

    🌱 update router file

--- projects/vue-3-study/src/router/index.js
+++ projects/vue-3-study/src/router/index.js
@@ -1,19 +1,25 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../pages/index.vue';
 import TodoList from '../pages/todoList/index.vue';
+import Todo from '../pages/todoList/_id.vue';
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
       path: '/',
-      name: 'HOME',
+      name: 'home',
       component: Home,
     },
     {
       path: '/todoList',
-      name: 'Todo List',
+      name: 'To do list',
       component: TodoList,
+    },
+    {
+      path: '/todoList/:id',
+      name: 'Todo',
+      component: Todo,
     },
   ],
 });
