bac5e0d5fcc9c986731d321c06131b9ff8b6c405
mnmhbbb <mnmhb64@gmail.com>
2024-04-08 23:32:02 +0900

    ✨ add a dynamic page feature in Next.js 14

--- next-14-test/src/app/test/[slug]/page.tsx
+++ next-14-test/src/app/test/[slug]/page.tsx
@@ -2,8 +2,20 @@ type Props = {
   params: { slug: string };
 };
 
+export function generateMetadata({ params }: Props) {
+  const { slug } = params;
+  return {
+    title: `Hello Next.js 14 | ${slug}`,
+  };
+}
+
 const TestLayoutPage = ({ params }: Props) => {
   return <h2>현재 경로: /test/{params.slug}</h2>;
 };
 
 export default TestLayoutPage;
+
+export function generateStaticParams() {
+  const test = ['winter', 'karina'];
+  return test.map((t) => ({ slug: t }));
+}
