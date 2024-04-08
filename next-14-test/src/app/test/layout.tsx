613f9185b6c5a670b57b07b2cfb1729f59727f2d
mnmhbbb <mnmhb64@gmail.com>
2024-04-08 23:41:44 +0900

    🧪  add a code to test pre-fetching in Next.js 14

--- next-14-test/src/app/test/layout.tsx
+++ next-14-test/src/app/test/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Link from 'next/link';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -13,10 +14,19 @@ export default function TestLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const list = ['first', 'second', 'third', 'fourth', 'fifth'];
+
   return (
     <html lang="en">
       <body className={inter.className}>
         <header>----Layout-----</header>
+        <nav className="flex gap-3">
+          {list.map((item, index) => (
+            <Link href={`/test/${item}`} key={index}>
+              {item}
+            </Link>
+          ))}
+        </nav>
         <section>{children}</section>
       </body>
     </html>
